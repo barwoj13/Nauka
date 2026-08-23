2db2cc2980e5674e2619ddb1cebe5507ef47ae40
barwoj13 <bw321728@student.polsl.pl>
2026-08-23 13:26:24 +0200

    Dziedzicznie diamentowe

--- nauka/nauka/main.cpp
+++ nauka/nauka/main.cpp
@@ -1,70 +1,66 @@
 #include <iostream>
 #include <string>
 
-class Pracownik
-{
-protected:
-    std::string imie;
-
-public:
-    Pracownik(const std::string& imie) : imie(imie) {}
-
-    // Wirtualny destruktor – gwarantuje wywo³anie w³aœciwego destruktora przy delete
-    virtual ~Pracownik() = default;
-
-    // Czysto wirtualna metoda (= 0) – klasa staje siê abstrakcyjna
-    virtual double obliczPensje() = 0;
-};
-
-class PracownikEstatowy : public Pracownik
+class Urzadzenie
 {
 private:
-    double pensjaPodstawowa;
+    std::string nazwa;
 
 public:
-    PracownikEstatowy(const std::string& imie, double pensjaPodstawowa)
-        : Pracownik(imie), pensjaPodstawowa(pensjaPodstawowa) {
+    Urzadzenie(const std::string& nazwa)
+        : nazwa(nazwa) {
     }
 
-    double obliczPensje() override
+    virtual ~Urzadzenie() = default;
+
+    std::string getNazwa() const { return nazwa; }
+
+    void wlacz()
     {
-        return pensjaPodstawowa;
+        std::cout << "Wlaczono urzadzenie: " << nazwa << std::endl;
     }
 };
 
-class PracownikGodzinowy : public Pracownik
+// 1. Kluczowe jest s³owo 'virtual' TUTAJ:
+class Drukarka : virtual public Urzadzenie
 {
 private:
-    double stawkaGodzinowa;
-    int liczbaGodzin;
+    int liczbaKartek;
 
 public:
-    PracownikGodzinowy(const std::string& imie, double stawkaGodzinowa, int liczbaGodzin)
-        : Pracownik(imie), stawkaGodzinowa(stawkaGodzinowa), liczbaGodzin(liczbaGodzin) {
+    Drukarka(const std::string& nazwa, int liczba)
+        : Urzadzenie(nazwa), liczbaKartek(liczba) {
     }
+};
 
-    double obliczPensje() override
-    {
-        return stawkaGodzinowa * liczbaGodzin;
+// 2. I TUTAJ:
+class Skaner : virtual public Urzadzenie
+{
+private:
+    int rozdzielcosc;
+
+public:
+    Skaner(const std::string& nazwa, int rozdzielcosc)
+        : Urzadzenie(nazwa), rozdzielcosc(rozdzielcosc) {
+    }
+};
+
+// 3. Dziedziczymy zwyczajnie publicznie z obu klas
+class UrzadzenieWielofunkcyjne : public Drukarka, public Skaner
+{
+public:
+    // Wypisanie konstruktora Urzadzenie(...) jest TUTAJ OBOWI¥ZKOWE, 
+    // poniewa¿ Urzadzenie jest dziedziczone wirtualnie!
+    UrzadzenieWielofunkcyjne(const std::string& nazwa, int kartki, int rozdzielcosc)
+        : Urzadzenie(nazwa), Drukarka(nazwa, kartki), Skaner(nazwa, rozdzielcosc) {
     }
 };
 
 int main()
 {
-    // Poprawne polimorficzne zarzadzanie pamiecia
-    Pracownik* tablica[] = {
-        new PracownikEstatowy("Jan", 4500.0),
-        new PracownikGodzinowy("Chrzan", 30.0, 160)
-    };
+    UrzadzenieWielofunkcyjne uw("HP All-In-One", 250, 1200);
 
-    for (int i = 0; i < 2; i++)
-    {
-        std::cout << tablica[i]->obliczPensje() << std::endl;
-    }
-
-    // Sprz¹tanie pamiêci – tutaj kluczowy jest wirtualny destruktor!
-    for (int i = 0; i < 2; i++)
-    {
-        delete tablica[i];
-    }
+    // Dzia³a bez b³êdu jednoznacznoœci (ambiguity error)!
+    // Istnieje tylko jeden podobiekt 'Urzadzenie'.
+    uw.wlacz();
 }
