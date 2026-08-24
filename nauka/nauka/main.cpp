894bfa65b18d57bc95503a13183991fb38a0ae23
barwoj13 <bw321728@student.polsl.pl>
2026-08-24 14:17:02 +0200

    Wkaźniki i referencje - zamiana wartości

--- nauka/nauka/main.cpp
+++ nauka/nauka/main.cpp
@@ -1,66 +1,37 @@
 #include <iostream>
 #include <string>
+#include <cassert>
 
-class Urzadzenie
+
+void swapByRef(int& a, int&b)
 {
-private:
-    std::string nazwa;
+	int bufor = a;
+	a = b;
+	b = bufor;
+}
 
-public:
-    Urzadzenie(const std::string& nazwa)
-        : nazwa(nazwa) {
-    }
-
-    virtual ~Urzadzenie() = default;
-
-    std::string getNazwa() const { return nazwa; }
-
-    void wlacz()
-    {
-        std::cout << "Wlaczono urzadzenie: " << nazwa << std::endl;
-    }
-};
-
-// 1. Kluczowe jest s³owo 'virtual' TUTAJ:
-class Drukarka : virtual public Urzadzenie
+void swapByPtr(int* a, int* b)
 {
-private:
-    int liczbaKartek;
-
-public:
-    Drukarka(const std::string& nazwa, int liczba)
-        : Urzadzenie(nazwa), liczbaKartek(liczba) {
-    }
-};
-
-// 2. I TUTAJ:
-class Skaner : virtual public Urzadzenie
-{
-private:
-    int rozdzielcosc;
-
-public:
-    Skaner(const std::string& nazwa, int rozdzielcosc)
-        : Urzadzenie(nazwa), rozdzielcosc(rozdzielcosc) {
-    }
-};
-
-// 3. Dziedziczymy zwyczajnie publicznie z obu klas
-class UrzadzenieWielofunkcyjne : public Drukarka, public Skaner
-{
-public:
-    // Wypisanie konstruktora Urzadzenie(...) jest TUTAJ OBOWI¥ZKOWE, 
-    // poniewa¿ Urzadzenie jest dziedziczone wirtualnie!
-    UrzadzenieWielofunkcyjne(const std::string& nazwa, int kartki, int rozdzielcosc)
-        : Urzadzenie(nazwa), Drukarka(nazwa, kartki), Skaner(nazwa, rozdzielcosc) {
-    }
-};
+	if (a == nullptr || b == nullptr) return;
+	int bufor = *a;
+	*a = *b;
+	*b = bufor;
+}
 
 int main()
 {
-    UrzadzenieWielofunkcyjne uw("HP All-In-One", 250, 1200);
+	int a = 3;
+	int b = 8;
 
-    // Dzia³a bez b³êdu jednoznacznoœci (ambiguity error)!
-    // Istnieje tylko jeden podobiekt 'Urzadzenie'.
-    uw.wlacz();
+	std::cout << "a: " << a << ", b: " << b << std::endl;
+
+	swapByRef(a, b);
+
+	std::cout << "a: " << a << ", b: " << b << std::endl;
+
+	swapByPtr(&a, &b);
+
+	std::cout << "a: " << a << ", b: " << b << std::endl;
+
+
 }
